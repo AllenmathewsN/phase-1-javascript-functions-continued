c22dd3a2da5be1e65c43a96e5faa6e299b5dc265
Allen <allenmathews416@gmail.com>
2022-08-23 17:01:30 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+saturdayFun("Watch movie");
+
+
+function mondayWork(work="go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+
+mondayWork();
+mondayWork("work from home");
+
+
+function wrapAdjective(allen){
+    return function(say){
+            return ( `You are ${allen}${say}${allen}!`);
+    };
+}
+wrapAdjective();
+wrapAdjective("%")("a dedicated programmer");
